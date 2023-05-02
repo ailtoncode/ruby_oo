c18f59346849657641589f76e3cf5000a5b3bd10
ailtoncode <contato.sc17@gmail.com>
2023-05-02 12:31:57 -0300

    refactor: altera forma de imprimir resultado

--- modules/namespace/calcular.rb
+++ modules/namespace/calcular.rb
@@ -1,22 +1,36 @@
 module Calcular
+  def self.setup(formatter)
+    @formatter = formatter
+  end
+
   def self.sum(v1, v2)
-    v1 + v2
+    self.puts(v1 + v2, @formatter)
   end
 
   def self.subt(v1, v2)
-    v1 - v2
+    self.puts(v1 + v2, @formatter)
   end
 
   def self.mult(v1, v2)
-    v1 * v2
+    self.puts(v1 + v2, @formatter)
   end
 
   def self.div(v1, v2)
-    v1 / v2
+    self.puts(v1 + v2, @formatter)
   end
+
+  
+  def self.puts (result, lambda)
+    lambda.call(result)
+  end
+  
+  private_class_method :puts
+
 end
 
-puts Calcular::sum(10, 5)
-puts Calcular::subt(32, 3)
-puts Calcular::mult(5, 5)
-puts Calcular::div(50, 5)
+resultado_formatado = -> (value){ puts "Resultado: #{value}" }
+Calcular::setup(resultado_formatado)
+Calcular::sum(10, 5)
+Calcular::subt(32, 3)
+Calcular::mult(5, 5)
+Calcular::div(50, 5)
