fd2d81105f96d55eac2557510680fdb08b4c1fc0
ailtoncode <contato.sc17@gmail.com>
2023-05-27 17:20:49 -0300

    feat: Mostra qual o dia da semana

---
+++ time/example4.rb
@@ -0,0 +1,10 @@
+time = Time.new(2022, 3, 1)
+
+puts "monday: #{time.monday?}"
+puts "Tuesday: #{time.tuesday?}"
+puts "Wednesday: #{time.wednesday?}"
+puts "Thursday: #{time.thursday?}"
+puts "Friday: #{time.friday?}"
+puts "Saturday: #{time.saturday?}"
+puts "Sunday: #{time.sunday?}"
+
